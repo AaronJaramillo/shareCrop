58b29022c397cbab098495c5f508447532c6152d
Aaron Jaramillo <aajaramillo95@gmail.com>
2017-06-05 12:55:56 -0700

    ERC20 + ICO contract

--- app/contracts/token.sol
+++ app/contracts/token.sol
@@ -6,49 +6,40 @@ pragma solidity ^0.4.8;
    // ERC Token Standard #20 Interface
    // https://github.com/ethereum/EIPs/issues/20
 contract ERC20Interface {
- // Get the total token supply
+
   function totalSupply() constant returns (uint256 totalSupply);
 
-  // Get the account balance of another account with address _owner
+
   function balanceOf(address _owner) constant returns (uint256 balance);
 
-  // Send _value amount of tokens to address _to
+
   function transfer(address _to, uint256 _value) returns (bool success);
 
-  // Send _value amount of tokens from address _from to address _to
   function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
 
-  // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
-  // If this function is called again it overwrites the current allowance with _value.
-  // this function is required for some DEX functionality
+
   function approve(address _spender, uint256 _value) returns (bool success);
 
- // Returns the amount which _spender is still allowed to withdraw from _owner
   function allowance(address _owner, address _spender) constant returns (uint256 remaining);
 
-  // Triggered when tokens are transferred.
   event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-  // Triggered whenever approve(address _spender, uint256 _value) is called.
+
   event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
    
 contract Token is ERC20Interface {
-  string public constant symbol = "FIXED";
-  string public constant name = "Example Fixed Supply Token";
+  string public constant symbol = "SCT";
+  string public constant name = "Share Crop Token";
   uint8 public constant decimals = 3;
   uint256 _totalSupply = 1000;
  
-  // Owner of this contract
   address public owner;
 
-  // Balances for each account
   mapping(address => uint256) balances;
 
-  // Owner of account approves the transfer of an amount to another account
   mapping(address => mapping (address => uint256)) allowed;
 
-  // Functions with this modifier can only be executed by the owner
   modifier onlyOwner() {
       if (msg.sender != owner) {
           throw;
@@ -56,7 +47,6 @@ contract Token is ERC20Interface {
       _;
   }
 
-  // Constructor
   function Token() {
       owner = msg.sender;
       balances[owner] = _totalSupply;
@@ -69,12 +59,12 @@ contract Token is ERC20Interface {
   function ownerBalance() constant returns (uint256 balance) {
     return balances[owner];
   }
-  // What is the balance of a particular account?
+
   function balanceOf(address _owner) constant returns (uint256 balance) {
       return balances[_owner];
   }
 
-  // Transfer the balance from owner's account to another account
+
   function transfer(address _to, uint256 _amount) returns (bool success) {
       if (balances[msg.sender] >= _amount 
           && _amount > 0
@@ -88,12 +78,6 @@ contract Token is ERC20Interface {
       }
   }
 
-  // Send _value amount of tokens from address _from to address _to
-  // The transferFrom method is used for a withdraw workflow, allowing contracts to send
-  // tokens on your behalf, for example to "deposit" to a contract address and/or to charge
-  // fees in sub-currencies; the command should fail unless the _from account has
-  // deliberately authorized the sender of the message via some mechanism; we propose
-  // these standardized APIs for approval:
   function transferFrom(
       address _from,
       address _to,
@@ -113,8 +97,7 @@ contract Token is ERC20Interface {
      }
  }
 
- // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
- // If this function is called again it overwrites the current allowance with _value.
+
  function approve(address _spender, uint256 _amount) returns (bool success) {
      allowed[msg.sender][_spender] = _amount;
      Approval(msg.sender, _spender, _amount);
